d17e67aa4a91193387572e87ffed0e81ae792527
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2024-09-12 19:48:07 +0300

    creating a new App: Employee

--- WEBAPP01_V01/WEBAPP01_DJANGOAPI/WEBAPP01_DJANGOAPI/settings.py
+++ WEBAPP01_V01/WEBAPP01_DJANGOAPI/WEBAPP01_DJANGOAPI/settings.py
@@ -40,9 +40,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
+    'EmployeeApp.apps.EmployeeappConfig',
+    'rest_framework'
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ORIGIN_WHITELIST = ('http://google.com')
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware'
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
