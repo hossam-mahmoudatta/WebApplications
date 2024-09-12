360999c2083aa1926ba351afc7768a08a9394bcb
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2024-09-12 22:20:52 +0300

    added the webapp01 react project

--- WEBAPP01_V01/WEBAPP01_DJANGOAPI/EmployeeApp/urls.py
+++ WEBAPP01_V01/WEBAPP01_DJANGOAPI/EmployeeApp/urls.py
@@ -1,14 +1,19 @@
 from django.urls import path
 from EmployeeApp import views
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
   # path('', home_view),                         # Root URL of the app
   # path('department/', department_view),        # App-level department URL
   # path('department/<int:id>/', department_detail_view),  # App-level detail URL
   
-  path(r'department/', views.departmentApi),
+  path(r'department', views.departmentApi),
   path(r'department/<int:id>/', views.departmentApi),
   
-  path(r'employee/', views.employeeApi),
-  path(r'employee/<int:id>/', views.employeeApi)
-]
+  path(r'employee', views.employeeApi),
+  path(r'employee/<int:id>/', views.employeeApi),
+  
+  path(r'Employee/SaveFile', views.SaveFile)
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- WEBAPP01_V01/WEBAPP01_DJANGOAPI/EmployeeApp/views.py
+++ WEBAPP01_V01/WEBAPP01_DJANGOAPI/EmployeeApp/views.py
@@ -6,6 +6,8 @@ from django.http import JsonResponse
 from EmployeeApp.models import Departments, Employees
 from EmployeeApp.serializers import DepartmentSerializer, EmployeeSerializer
 
+from django.core.files.storage import default_storage
+
 # Create your views here.
 @csrf_exempt
 def departmentApi(request, id=0):
@@ -69,3 +71,11 @@ def employeeApi(request, id=0):
     employee.delete()
     employee_serializer = EmployeeSerializer(employee, data=employee_data)
     return JsonResponse("Deleted Successfully.", safe=False)
+  
+  
+@csrf_exempt
+def SaveFile(request):
+  file = request.FILES['myFile']
+  file_name = default_storage.save(file.name, file)
+  
+  return JsonResponse(file_name, safe = False);
