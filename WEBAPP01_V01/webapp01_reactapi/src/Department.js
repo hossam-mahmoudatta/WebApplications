2aa8a1fc6dbfccb877655d4b7a96f712fd825fd6
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2024-09-12 23:49:21 +0300

    edited in the Departments File

--- WEBAPP01_V01/webapp01_reactapi/src/Department.js
+++ WEBAPP01_V01/webapp01_reactapi/src/Department.js
@@ -1,10 +1,15 @@
 import React,{Component} from 'react';
+import {Table} from 'react-bootstrap';
+
+// import {Button,ButtonToolbar} from 'react-bootstrap';
+// import {AddDepModal} from './AddDepModal';
+// import {EditDepModal} from './EditDepModal';
 
 export class Department extends Component{
 
   constructor(props){
     super(props);
-    this.state = {deps:[], addModalShow:false, editModalShow:false}
+    this.state = {deps:[]}
   }
 
   refreshList(){
@@ -25,10 +30,83 @@ export class Department extends Component{
 
 
   render(){
+    const {deps} = this.state;
     return(
-      <div className="mt-5 d-flex justify-content-left">
-        This is Department page.
+      <div >
+        <Table className="mt-4" striped bordered hover size="sm">
+          <thead>
+              <tr>DepartmentId</tr>
+              <tr>DepartmentName</tr>
+              <tr>Options</tr>
+          </thead>
+          <tbody>
+            {deps.map(dep =>
+              <tr key={dep.DepartmentId}>
+                <td>{dep.DepartmentId}</td>
+                <td>{dep.DepartmentName}</td>
+                <td>Edit / Delete</td>
+              </tr>)
+            }
+          </tbody>
+        </Table>
       </div>
     )
   }
 }
+
+
+
+// render(){
+//   const {deps, depid, depname} = this.state;
+//   let addModalClose =()=> this.setState({addModalShow:false});
+//   let editModalClose =()=> this.setState({editModalShow:false});
+//   return(
+//     <div >
+//       <Table className="mt-4" striped bordered hover size="sm">
+//         <thead>
+//           <tr>
+//             <th>DepartmentId</th>
+//             <th>DepartmentName</th>
+//             <th>Options</th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           {deps.map(dep =>
+//             <tr key={dep.DepartmentId}>
+//               <td>{dep.DepartmentId}</td>
+//               <td>{dep.DepartmentName}</td>
+//               <td>
+//                 <ButtonToolbar>
+//                   <Button className="mr-2" variant="info"
+//                   onClick={() => this.setState({editModalShow:true,
+//                   depid:dep.DepartmentId,depname:dep.DepartmentName})}>
+//                     Edit
+//                   </Button>
+//                   <Button className="mr-2" variant="danger"
+//                   onClick={() => this.deleteDep(dep.DepartmentId)}>
+//                     Delete
+//                   </Button>
+
+//                   <EditDepModal show  = {this.state.editModalShow}
+//                               onHide  = {editModalClose}
+//                               depid   = {depid}
+//                               depname = {depname}/>
+//                 </ButtonToolbar>
+//               </td>
+//             </tr>)
+//           }
+//         </tbody>
+//       </Table>
+
+//       <ButtonToolbar>
+//         <Button variant='primary'
+//           onClick={()=>this.setState({addModalShow:true})}>
+//           Add Department
+//         </Button>
+
+//         <AddDepModal show={this.state.addModalShow}
+//         onHide={addModalClose}/>
+//       </ButtonToolbar>
+//     </div>
+//   )
+// }
