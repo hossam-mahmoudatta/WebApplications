009c11183e4af015674489586a0f526d50b6346a
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2024-09-13 12:06:26 +0300

    changed a display name in the admin page

--- WEBAPP01_V01/WEBAPP01_DJANGOAPI/EmployeeApp/apps.py
+++ WEBAPP01_V01/WEBAPP01_DJANGOAPI/EmployeeApp/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class EmployeeappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'EmployeeApp'
+    verbose_name = 'Employee Management System'  # Custom app name shown in admin
