baed76b529457da8f0b976e9ebe5b14e3a55e16a
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2024-09-13 11:38:22 +0300

    edited some stuff in the admin view

--- WEBAPP01_V01/WEBAPP01_DJANGOAPI/EmployeeApp/admin.py
+++ WEBAPP01_V01/WEBAPP01_DJANGOAPI/EmployeeApp/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Employees, Departments  # Import your model
 
 # Register your models here.
+
+
+# Register the model with the admin site
+class EmployeeAdmin(admin.ModelAdmin):
+  list_display = ('EmployeeId',
+                  'EmployeeName',
+                  'Department',
+                  'DateOfJoining',
+                  'PhotoFileName')  # Customize columns in the list view
+  search_fields = ('EmployeeId',
+                  'EmployeeName',
+                  'Department',
+                  'DateOfJoining',
+                  'PhotoFileName')  # Add search functionality
+  
+class DepartmentAdmin(admin.ModelAdmin):
+  list_display = ('DepartmentId',
+                  'DepartmentName')  # Customize columns in the list view
+  search_fields = ('DepartmentId',
+                  'DepartmentName')  # Add search functionality
+
+admin.site.register(Employees,
+                    EmployeeAdmin)
+
+admin.site.register(Departments,
+                    DepartmentAdmin)
+
--- WEBAPP01_V01/WEBAPP01_DJANGOAPI/EmployeeApp/models.py
+++ WEBAPP01_V01/WEBAPP01_DJANGOAPI/EmployeeApp/models.py
@@ -5,10 +5,17 @@ from django.db import models
 class Departments(models.Model):
   DepartmentId = models.AutoField(primary_key=True)
   DepartmentName = models.CharField(max_length=100)
-  
+  class Meta:
+    verbose_name = 'Department'  # Singular form
+    verbose_name_plural = 'Departments'  # Plural form (without an 's')
+    
+
 class Employees(models.Model):
   EmployeeId = models.AutoField(primary_key=True)
   EmployeeName = models.CharField(max_length=100)
   Department = models.CharField(max_length=100)
   DateOfJoining = models.DateField()
   PhotoFileName = models.CharField(max_length=100)
+  class Meta:
+    verbose_name = 'Employee'  # Singular form
+    verbose_name_plural = 'Employees'  # Plural form (without an 's')
