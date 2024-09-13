f0d5b94b59401d334e6bfb473332d70ec9732753
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2024-09-13 12:00:55 +0300

    Edited in how the columns display

--- WEBAPP01_V01/WEBAPP01_DJANGOAPI/EmployeeApp/admin.py
+++ WEBAPP01_V01/WEBAPP01_DJANGOAPI/EmployeeApp/admin.py
@@ -17,11 +17,22 @@ class EmployeeAdmin(admin.ModelAdmin):
                   'DateOfJoining',
                   'PhotoFileName')  # Add search functionality
   
+  # # Define methods to change the display names of the columns
+  # def first_name_display(self, obj):
+  #   return obj.first_name
+  #   first_name_display.short_description = 'First Name'  # Custom column title
+
+  # def department_display(self, obj):
+  #   return obj.department.department_name
+  #   department_display.short_description = 'Department Name'  # Custom column title
+  
 class DepartmentAdmin(admin.ModelAdmin):
   list_display = ('DepartmentId',
                   'DepartmentName')  # Customize columns in the list view
   search_fields = ('DepartmentId',
                   'DepartmentName')  # Add search functionality
+
+
 
 admin.site.register(Employees,
                     EmployeeAdmin)
--- WEBAPP01_V01/WEBAPP01_DJANGOAPI/EmployeeApp/models.py
+++ WEBAPP01_V01/WEBAPP01_DJANGOAPI/EmployeeApp/models.py
@@ -3,19 +3,19 @@ from django.db import models
 # Create your models here.
 
 class Departments(models.Model):
-  DepartmentId = models.AutoField(primary_key=True)
-  DepartmentName = models.CharField(max_length=100)
+  DepartmentId = models.AutoField(primary_key=True, verbose_name='ID')
+  DepartmentName = models.CharField(max_length=100, verbose_name='Name')
   class Meta:
     verbose_name = 'Department'  # Singular form
     verbose_name_plural = 'Departments'  # Plural form (without an 's')
     
 
 class Employees(models.Model):
-  EmployeeId = models.AutoField(primary_key=True)
-  EmployeeName = models.CharField(max_length=100)
-  Department = models.CharField(max_length=100)
-  DateOfJoining = models.DateField()
-  PhotoFileName = models.CharField(max_length=100)
+  EmployeeId = models.AutoField(primary_key=True, verbose_name='ID')
+  EmployeeName = models.CharField(max_length=100, verbose_name='Name')
+  Department = models.CharField(max_length=100, verbose_name='Department')
+  DateOfJoining = models.DateField(verbose_name='Date')
+  PhotoFileName = models.CharField(max_length=100, verbose_name='Photo')
   class Meta:
     verbose_name = 'Employee'  # Singular form
     verbose_name_plural = 'Employees'  # Plural form (without an 's')
